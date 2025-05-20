cd95e0a186496cd7c1c2db859ffab5a46eab97ed
temuulen26 <temuulentemm@gmail.com>
2025-05-20 16:40:57 +0800

    docker relate file

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,7 +13,7 @@ class TestApp(unittest.TestCase):
         data = response.get_json()
         self.assertEqual(data['result'], '11')
     def test_case_1(self):
-        payload = {"a": 11 , "b":8, "operator":"+"}
+        payload = {"a": 11 , "b":8, "operator":"-"}
         response = self.app.post('/lab15/operation/', data=json.dumps(payload), content_type = 'application/json')
         self.assertIn(response.status_code, [200, 201])
         data = response.get_json()
