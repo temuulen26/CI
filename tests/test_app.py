c9d41168470fdf286cb81ee870e6b334afa34c00
temuulen26 <temuulentemm@gmail.com>
2025-05-20 16:48:37 +0800

    docker re

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,7 +12,7 @@ class TestApp(unittest.TestCase):
         self.assertIn(response.status_code, [200, 201])
         data = response.get_json()
         self.assertEqual(data['result'], '11')
-    def test_case_1(self):
+    def test_case_2(self):
         payload = {"a": 11 , "b":8, "operator":"-"}
         response = self.app.post('/lab15/operation/', data=json.dumps(payload), content_type = 'application/json')
         self.assertIn(response.status_code, [200, 201])
