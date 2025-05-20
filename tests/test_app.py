530edd189d48dba275de0f91d221a3a088a920c0
temuulen26 <temuulentemm@gmail.com>
2025-05-20 16:49:38 +0800

    docker relate file

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,17 +6,20 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-    def test_case_1(self):
-        payload = {"a": 5 , "b":6, "operator":"+"}
-        response = self.app.post('/lab15/operation/', data=json.dumps(payload), content_type = 'application/json')
+
+    def test_addition(self):
+        payload = {"a": 5, "b": 6, "operator": "+"}
+        response = self.app.post('/lab15/operation/', data=json.dumps(payload), content_type='application/json')
         self.assertIn(response.status_code, [200, 201])
         data = response.get_json()
-        self.assertEqual(data['result'], '11')
-    def test_case_2(self):
-        payload = {"a": 11 , "b":8, "operator":"-"}
-        response = self.app.post('/lab15/operation/', data=json.dumps(payload), content_type = 'application/json')
+        self.assertEqual(data['result'], 11)
+
+    def test_subtraction(self):
+        payload = {"a": 11, "b": 8, "operator": "-"}
+        response = self.app.post('/lab15/operation/', data=json.dumps(payload), content_type='application/json')
         self.assertIn(response.status_code, [200, 201])
         data = response.get_json()
-        self.assertEqual(data['result'], '3')
+        self.assertEqual(data['result'], 3)
+
 if __name__ == '__main__':
     unittest.main()
